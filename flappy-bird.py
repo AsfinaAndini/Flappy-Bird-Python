11dd4b1a76babc9dfadbbeef73f0684d43fbbe0e
AsfinaAndini <128123181+AsfinaAndini@users.noreply.github.com>
2024-05-04 21:07:37 +0700

    Add Bird class for sprite representation

--- flappy-bird.py
+++ flappy-bird.py
@@ -21,6 +21,21 @@ scroll_speed = 4
 bg = pygame.image.load('img/bg.png')
 ground_img = pygame.image.load('img/ground.png')
 
+class Bird(pygame.sprite.Sprite):
+	def __init__(self, x, y):
+		pygame.sprite.Sprite.__init__(self)
+		self.images = []
+		self.index = 0
+		self.counter = 0
+		for num in range(1, 4):
+			img = pygame.image.load(f'img/bird{num}.png')
+			self.images.append(img)
+		self.image = self.images[self.index]
+		self.rect = self.image.get_rect()
+		self.rect.center = [x, y]
+
+
+
 run = True
 while run:
 
